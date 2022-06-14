c72269b10f339b7b0769b22453da7479daffc3b7
Stavros Mantzouneas <930338+mantzoun@users.noreply.github.com>
2022-06-14 16:17:30 +0300

    move sources in new module

---
+++ pr_sources.py
@@ -0,0 +1,4 @@
+sources = [ # source url/path,              source title,   type
+           ["/path/to/playslist.m3u",       "List Title",   "list"],
+           ["http://url_of.stream",         "Stream Title", "stream"],
+          ]
